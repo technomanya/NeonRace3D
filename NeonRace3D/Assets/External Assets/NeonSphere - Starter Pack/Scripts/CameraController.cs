6373a609e0547ed85b667cb3dea34bc49b6776de
technomanya <38087296+technomanya@users.noreply.github.com>
2020-06-05 17:59:43 +0300

    new

--- NeonRace3D/Assets/External Assets/NeonSphere - Starter Pack/Scripts/CameraController.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d9974fd78be992b20ae037a10b4680f14864de7dee7de676ccc935696189f3ad
-size 484
--- NeonRace3D/Assets/External Assets/NeonSphere - Starter Pack/Scripts/GameManager.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:95923cecd90ba2c0072ff601df636bf5977e007e0ff61ef09f268f9be0cb706f
-size 3644
